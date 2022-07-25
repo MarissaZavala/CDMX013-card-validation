dad3c97a4cf38ea003ff8c1d9301893c5c2ccb47
MarissaZavala <107640427+MarissaZavala@users.noreply.github.com>
2022-07-25 18:14:11 -0500

    validation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import validator from './validator.js';
-let button = document.getElementById ("button")
-button.addEventListener ("click", function (validCard) {
-let cardNumber= document.getElementById (cardNumber).value;
+let button = document.getElementById ("buttonSend")//declarar button llamar id "button" desde index.html
+button.addEventListener ("click", function (e) {
+   e.preventDefault ()
+let cardNumber= document.getElementById ("cardNumber").value;
+
+let validacion= validator.isValid(cardNumber);
+//let tarjetaOculta = validator.maskify(cardNumber);
+if (validacion){
+  document.getElementById("results").innerHTML = "Tu tarjeta es valida!" + "<br/>" ;
+ 
+  
+}else{ 
+  document.getElementById("results").innerHTML = "Tu tarjeta es invalida!" + "<br/>" ; 
+ 
 }
 console.log(validator);
+
+});
--- src/validator.js
+++ src/validator.js
@@ -1,7 +1,36 @@
 const validator = {
-function validCard(cardNumber) {
-  let divCard = cardNumber.split("")
+  isValid: function (cardNumber) {
+    let numCard = cardNumber.split("").reverse().map(Number)//Se declara la variable dividiendo los elementos de la variable cardNumber  let longArreglo = (numCard.length);
+    //console.log(numCard)
+  
+let sum = 0; //verifica que la suma final sea 0 //
+
+for (let i = 0; i < numCard.length; i++) {
+  if (i % 2 === 1) {
+    let multiplicar = numCard[i] * 2
+    console.log(multiplicar)
+    if (multiplicar > 9) {
+      let resultadoFinal = multiplicar - 9
+      sum += resultadoFinal
+    } else {
+      sum += multiplicar;                 
+//console.log(multiplicar);
+    }
+  } else /*condicional para posiciones pares*/ {
+    sum += numCard[i];}
 }
-};
+
+let results = sum % 10; //nueva variable para resultado final de la operación// 
+        if (results === 0) {
+            return true;
+        } else {
+            return false;
+        }
+      }}
+      console.log(results)
+    
+  
 
 export default validator;
+
+
